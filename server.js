97a725dfed9b14f28efb6ff616ccca1dd924b0f3
Bader Alsabah <bader@badercodes.com>
2022-09-18 10:21:10 +0300

    fixed unneccesary body-parser import

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 // setting up server
-const { urlencoded } = require("body-parser");
 const express = require("express");
 const server = express();
 const port = 3030;
 
 // server needs to be able to parse data
 server.use(express.json());
-server.use(urlencoded({ extended: true }));
+server.use(express.urlencoded({ extended: true }));
 
 // holder for index for now (GET request handler)
 server.get("/", (req, res) => res.send("<h1>You landed on homepage</h1>"));
 
 // test post route to verify that server can read posted requests
 server.post("/add", (req, res) => {
-  const name = req.body.name;
+  const obj = {
+    name: req.body.name,
+    age: req.body.age,
+    gender: req.body.gender,
+  };
   console.log(req.body);
-  res.send("Welcome to our site Mr " + name);
+  res.send("Welcome to our site Mr " + obj.name);
 });
 
 // listen - last command on server - any commands afer this wont execute
