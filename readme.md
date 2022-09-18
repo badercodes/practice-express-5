acde50a0cf30c310f3a050d6043210e93d9f590f
Bader Alsabah <bader@badercodes.com>
2022-09-18 10:15:35 +0300

    setup the server to handle get and post requests

---
+++ readme.md
@@ -0,0 +1,9 @@
+1 - create server.js
+2 - npm init
+3 - git init
+4 - Setup git repo
+5 - install express & nodemon
+6 - require and invoke express
+7 - Listen on some random port
+8 - Create a GET route and verify its being handled
+9 - Configure built in body-parser data that gets posted
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-// entry point to the app
+// setting up server
+const { urlencoded } = require("body-parser");
+const express = require("express");
+const server = express();
+const port = 3030;
+
+// server needs to be able to parse data
+server.use(express.json());
+server.use(urlencoded({ extended: true }));
+
+// holder for index for now (GET request handler)
+server.get("/", (req, res) => res.send("<h1>You landed on homepage</h1>"));
+
+// test post route to verify that server can read posted requests
+server.post("/add", (req, res) => {
+  const name = req.body.name;
+  console.log(req.body);
+  res.send("Welcome to our site Mr " + name);
+});
+
+// listen - last command on server - any commands afer this wont execute
+server.listen(port, () => console.log(`Server is connected on port ${port}`));
